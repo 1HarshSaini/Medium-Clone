253c9be33f72da114c52dcfb39b6f568f72d11fa
NAMAN SHARMA <78334678+NAMAN507@users.noreply.github.com>
2023-06-06 09:40:36 +0530

    Add files via upload

---
+++ git/backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const url ="mongodb+srv://naman1949be20:naman@cluster0.i7awick.mongodb.net/";
+module.exports.connect = () => {
+  mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      // useFindAndModify: false,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+    })
+    .then(() => console.log("MongoDB is connected successfully"))
+    .catch((err) => console.log("Error: ", err));
+};
